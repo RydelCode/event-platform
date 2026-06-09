eea451ed1da3b3b7d85f81f663679c3ffa229b92
RydelCode <piotr.rydel92@gmail.com>
2026-06-09 14:29:28 +0200

    feat(backend): process registration events async with RabbitMQ

--- backend/src/Application/Event/RegisterForEvent/Message/RegistrationCreatedHandler.php
+++ backend/src/Application/Event/RegisterForEvent/Message/RegistrationCreatedHandler.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 
 namespace App\Application\Event\RegisterForEvent\Message;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
-class RegistrationCreatedHandler
+final readonly class RegistrationCreatedHandler
 {
+    public function __construct(
+        private LoggerInterface $logger,
+    ) {
+    }
+
     public function __invoke(RegistrationCreated $message): void
     {
-        var_dump($message->getRegistrationId());
+        $this->logger->info('Registration created message handled.', [
+            'registrationId' => $message->getRegistrationId(),
+        ]);
     }
 }
